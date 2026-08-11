2219256f454d965357f3e46bc70b1976f8526eec
Wazzabeam98 <liamworrall18@hotmail.com>
2026-08-11 06:14:46 +0100

    Force dynamic rendering for homes page

--- app/homes/[id]/page.tsx
+++ app/homes/[id]/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import React from 'react'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers';
