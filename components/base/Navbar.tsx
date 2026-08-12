019572ca0456b46446e6d58d915b9fc6d09a31a1
Wazzabeam98 <liamworrall18@hotmail.com>
2026-08-12 16:41:35 +0100

    douuble

--- components/base/Navbar.tsx
+++ components/base/Navbar.tsx
@@ -1,30 +1,30 @@
-import NavMenu from './NavMenu'
+export const dynamic = 'force-dynamic';
+
+import NavMenu from '@/components/base/NavMenu';
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import Link from 'next/link'
-import Logo from './Logo';
-import SearchSheet from '../common/SearchSheet';
+import Link from 'next/link';
+import Logo from '@/components/base/Logo';
 
 const Navbar = async () => {
     const supabase = createServerComponentClient({ cookies });
     const { data } = await supabase.auth.getSession();
     
     return (
-        <div className='flex items-center justify-between md:px-10 border-b-[1px]'>
-            <div className='hidden md:block'>
-                <Logo />
+        <nav className='w-full border-b bg-white sticky top-0 z-50'>
+            <div className='max-w-7xl mx-auto px-6 md:px-10 h-20 flex items-center justify-between'>
+                <div className='flex items-center'>
+                    <Logo />
+                </div>
+                <div className='flex items-center space-x-6'>
+                    <Link href="/addhome" className="text-sm font-semibold hover:bg-slate-100 rounded-full py-2 px-4 transition text-slate-800">
+                        Become a host
+                    </Link>
+                    <NavMenu session={data?.session?.user} />
+                </div>
             </div>
-            <div className='w-full md:w-auto'>
-                <SearchSheet session={data?.session?.user} />
-            </div>
-            <div className='hidden md:flex items-center space-x-4'>
-                <Link href="/addhome" className="text-sm font-semibold">
-                    Add homes
-                </Link>
-                <NavMenu session={data?.session?.user} />
-            </div>
-        </div>
-    )
-}
+        </nav>
+    );
+};
 
-export default Navbar
+export default Navbar;
