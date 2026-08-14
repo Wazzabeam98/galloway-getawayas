23d3cfe1493a6024e6dfb8206fb96617a1f22276
Wazzabeam98 <liamworrall18@hotmail.com>
2026-08-14 11:22:30 +0100

    Replace GitHub login with Facebook login

--- components/auth/SocialSignUp.tsx
+++ components/auth/SocialSignUp.tsx
@@ -7,9 +7,9 @@ import { toast } from 'react-toastify';
 const SocialSignUp = () => {
     const supabase = createClientComponentClient();
 
-    const githubLogin = async () => {
+    const facebookLogin = async () => {
         const { error } = await supabase.auth.signInWithOAuth({
-            provider: 'github',
+            provider: 'facebook',
             options: {
                 redirectTo: `${location.origin}/auth/callback`
             }
@@ -37,9 +37,11 @@ const SocialSignUp = () => {
                 <Image src='/images/google.png' alt='google_logo' className='mr-5' height={25} width={25} />
                 Continue with Google
             </Button>
-            <Button variant='outline' className='w-full mt-5' onClick={githubLogin}>
-                <Image src='/images/github.png' alt='github_logo' className='mr-5' height={25} width={25} />
-                Continue with GitHub
+            <Button variant='outline' className='w-full mt-5' onClick={facebookLogin}>
+                <svg className="mr-5" height={25} width={25} viewBox="0 0 24 24">
+                    <path fill="#1877F2" d="M24 12.07C24 5.4 18.63 0 12 0S0 5.4 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.49h3.05V9.41c0-3.02 1.79-4.69 4.53-4.69 1.31 0 2.68.24 2.68.24v2.97h-1.51c-1.49 0-1.96.93-1.96 1.89v2.25h3.33l-.53 3.49h-2.8V24C19.61 23.1 24 18.1 24 12.07z"/>
+                </svg>
+                Continue with Facebook
             </Button>
         </>
     )
